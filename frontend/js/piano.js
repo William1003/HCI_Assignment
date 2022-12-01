cdd85d0ce4bd55d5afcd0568fb93dba885ad2389
William <13313780609@163.com>
2022-12-01 17:34:48 +0800

    .

--- frontend/js/piano.js
+++ frontend/js/piano.js
@@ -158,77 +158,40 @@ document.onkeydown = function (e) {
 	switch (keyCode) {
 		case 65:	// a
 		case 97:
-		case 81:	// q
-		case 113:
-		case 90:	// z
-		case 122:
 			playSound(60);
-			break;
-		case 87:	// w
-		case 119:
-			playSound(61);
+			addRecord('do');
 			break;
 		case 83:	// s
 		case 115:
-		case 88:	// x
-		case 120:
+			// case 88:	// x
+			// case 120:
 			playSound(62);
+			addRecord('re');
 			break;
-		case 69:	// e
-		case 101:
-			playSound(63);
-			break;
-		case 67:	// c
-		case 99:
 		case 68:	// d
 		case 100:
 			playSound(64);
+			addRecord('me');
 			break;
-		case 70:	// f
-		case 102:
-		case 82:	// r
-		case 114:
-		case 86:	// v
-		case 118:
+		case 70:
+		case 94:
 			playSound(65);
+			addRecord("fa");
 			break;
-		case 84:	// t
-		case 116:
-			playSound(66);
-			break;
-		case 66:	// b
-		case 98:
-		case 71:	// g
-		case 103:
-			playSound(67);
-			break;
-		case 89:	// y
-		case 121:
-			playSound(68);
-			break;
-		case 72:	// h
-		case 104:
-		case 78:	// n
-		case 110:
-			playSound(69);
-			break;
-		case 85:	// u
-		case 117:
-			playSound(70);
-			break;
-		case 77:	// m
-		case 109:
-		case 74:	// j
+		case 74:	
 		case 106:
-			playSound(71);
+			playSound(67);
+			addRecord("so");
 			break;
-		case 75:	// k
+		case 75:	
 		case 107:
-		case 73:	// i
-		case 105:
-		case 44:	// ,
-		case 188:
-			playSound(72);
+			playSound(69);
+			addRecord("la");
+			break;
+		case 76:	
+		case 108:
+			playSound(71);
+			addRecord("xi");
 			break;
 		default:
 			break;
